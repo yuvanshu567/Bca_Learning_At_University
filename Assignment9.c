87f47aca5f2aa6c3e83f63080d96fceba71299b5
yuvanshu567 <128624159+yuvanshu567@users.noreply.github.com>
2023-04-17 23:53:21 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,36 @@
 //.       m = 1
 //.       n = 1
 
-int main()
+#include<stdio.h>
+#include<string.h>
+void main()
 {
-  
-  return 0;
+    char s[100];
+    int count;
+    int a;
+   printf("Enter the string : ");
+    gets(s);
+    for(int i=0;i<strlen(s);i++)
+    {
+        a=1;
+        count=0;
+        for(int j=0;j<i;j++)
+        {
+            if(s[i]==s[j] && i!=j)
+            {
+                a=0;
+                break;
+            }
+        }
+        if(a==1)
+        {
+            for(int j=0;j<strlen(s);j++)
+            {
+                if(s[i]==s[j])
+                    count++;
+            }
+            printf("%c : %d\n",s[i],count);
+        }
+    }
+
 }
