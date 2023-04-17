b03a7790f34b8636a686364a2f3bf15119073756
yuvanshu567 <128624159+yuvanshu567@users.noreply.github.com>
2023-04-17 23:57:20 +0530

    Update Assignment13.c

--- Assignment13.c
+++ Assignment13.c
@@ -6,7 +6,50 @@
 
 //after showing result the main menu should appear again.
 
-int main()
+#include<stdio.h>
+void main()
 {
-  return 0;
+    int choice;
+    float num1,num2,res;
+    char ch;
+
+    do
+    {
+    printf("Press 1 for addition \nPress 2 for subtraction \nPress 3 for multiplication\nPress 4 for division\n");
+    printf("Enter your choice : ");
+    scanf("%d",&choice);
+    switch(choice)
+        {
+            case 1:
+                printf("Enter two numbers : ");
+                scanf("%f %f",&num1,&num2);
+                res=num1+num2;
+                break;
+            case 2:
+                printf("Enter two numbers : ");
+                scanf("%f %f",&num1,&num2);
+                res=num1-num2;
+                break;
+            case 3:
+                printf("Enter two numbers : ");
+                scanf("%f %f",&num1,&num2);
+                res=num1*num2;
+                break;
+            case 4:
+                printf("Enter two numbers : ");
+                scanf("%f %f",&num1,&num2);
+                res=num1/num2;
+                break;
+
+        }
+    printf("Output of the operation is : %f\n",res);
+    printf("Do you want to continue : ");
+    fflush(stdin);
+    scanf("%c",&ch);
+    printf("\n\n\n");
+
+    }
+
+
+    while(ch=='y' || ch=='Y');
 }
