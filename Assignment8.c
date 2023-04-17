e06c0a74f92359412e1d819c415e6754dd189d28
yuvanshu567 <128624159+yuvanshu567@users.noreply.github.com>
2023-04-17 23:52:08 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,33 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
+#include<stdio.h>
+#include<string.h>
+void main()
 {
-  return 0;
+    char s[100];
+    int l;
+    printf("Enter the string : ");
+    gets(s);
+    l=strlen(s); // length of string
+    for(int i=0;i<l;i++)
+    {
+        for(int j=i+1;j<l;j++)
+        {
+            if(s[i]>s[j])
+            {
+                swap(&s[i],&s[j]);
+            }
+        }
+    }
+    printf("%s",s);
+
+}
+
+swap(char*s1 ,char*s2 )
+{
+    char temp;
+    temp=*s1;
+    *s1=*s2;
+    *s2=temp;
 }
